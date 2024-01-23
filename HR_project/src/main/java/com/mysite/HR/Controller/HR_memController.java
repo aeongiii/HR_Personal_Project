bd2bcdf51a8da2db6f1bd56bc26f19f90bba2523
aeongiii <151815152+aeongiii@users.noreply.github.com>
2024-01-23 11:43:03 +0900

    20240123 2-11 URL 프리픽스 추가

--- HR_project/src/main/java/com/mysite/HR/Controller/HR_memController.java
+++ HR_project/src/main/java/com/mysite/HR/Controller/HR_memController.java
@@ -6,12 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mysite.HR.Entity.HR_mem;
 import com.mysite.HR.Service.MemService;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/HR3")
 @RequiredArgsConstructor	//  final이 붙은 속성을 포함하는 생성자를 자동으로 만듦 -> (= memRepository 객체 자동으로 주입됨)
 @Controller
 public class HR_memController {
@@ -19,7 +21,7 @@ public class HR_memController {
 	private final MemService memService;
 	
 	// 메인화면
-	@GetMapping("/HR3/list")	
+	@GetMapping("/list")	
 //	@ResponseBody    이 애너테이션이 있으면 mem_list라는 html을 리턴하는게 아니라 "mem_list" 문자 자체를 출력함
 	public String list(Model model) {	// model 객체 따로 생성할 필요 없이 매개변수 지정만 하면 스프링부트에서 생성됨
 		List<HR_mem> memList = this.memService.getList();
@@ -28,7 +30,7 @@ public class HR_memController {
 	}
 	
 	// 메인화면에서 이름 클릭 시 사원 상세정보 보기
-	@GetMapping(value = "/HR3/detail/{EmployeeID}")	
+	@GetMapping(value = "/detail/{EmployeeID}")	
 	public String detail(Model model, @PathVariable("EmployeeID") String id) {
 		HR_mem mem1 = this.memService.getHR_mem(id);
 		model.addAttribute("member", mem1);
